95eb4908c70892c38c3d5b8644e184d1f54c1144
592210455 <592210455@qq.com>
2025-12-26 18:50:05 +0800

    1

--- nextkit-nextjs-prisma-mongodb/src/app/(DashboardLayout)/utilities/table/page.tsx
+++
@@ -1,9 +0,0 @@
-"use client"
-import { ProductPerformance } from "@/app/components/dashboard/ProductPerformance";
-const page = () => {
-    return (
-       <ProductPerformance/>
-    )
-}
-
-export default page
